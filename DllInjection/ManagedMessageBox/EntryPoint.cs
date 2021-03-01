ab81ff4ac3d21cac76b49f256349106f276ecb52
Thomas Leiber <tomthebig1337@gmail.com>
2021-03-01 19:05:07 +0100

    * added managed test library

---
+++ DllInjection/ManagedMessageBox/EntryPoint.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace ManagedMessageBox
+{
+    public class EntryPoint
+    {
+        [DllExport("DllMain")]
+        public static void DllMain()
+        {
+            MessageBox.Show("Moin");
+        }
+    }
+}
