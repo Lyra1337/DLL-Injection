9b58d2844b64b4d08f4e8a6d26c4c261fa47bba6
Thomas Leiber <tomthebig1337@gmail.com>
2021-03-01 19:04:35 +0100

    * refactoring

---
+++ DllInjection/DllInjection/Native.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllInjection
+{
+    public static partial class Native
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(
+            ProcessAccessFlags processAccess,
+            bool bInheritHandle,
+            int processId
+        );
+
+        public static IntPtr OpenProcess(Process proc, ProcessAccessFlags flags)
+        {
+            return OpenProcess(flags, false, proc.Id);
+        }
+
+        // VirtualFreeEx signture  https://www.pinvoke.net/default.aspx/kernel32.virtualfreeex
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        public static extern bool VirtualFreeEx(
+            IntPtr hProcess,
+            IntPtr lpAddress,
+            int dwSize,
+            AllocationType dwFreeType
+        );
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        public static extern IntPtr VirtualAllocEx(
+            IntPtr hProcess,
+            IntPtr lpAddress,
+            IntPtr dwSize,
+            AllocationType flAllocationType,
+            MemoryProtection flProtect
+        );
+
+        // WriteProcessMemory signture https://www.pinvoke.net/default.aspx/kernel32/WriteProcessMemory.html
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern bool WriteProcessMemory(
+            IntPtr hProcess,
+            IntPtr lpBaseAddress,
+            [MarshalAs(UnmanagedType.AsAny)] object lpBuffer,
+            int dwSize,
+            out IntPtr lpNumberOfBytesWritten
+        );
+
+        // GetProcAddress signture https://www.pinvoke.net/default.aspx/kernel32.getprocaddress
+        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+        public static extern IntPtr GetProcAddress(
+            IntPtr hModule,
+            string procName
+        );
+
+        // GetModuleHandle signture http://pinvoke.net/default.aspx/kernel32.GetModuleHandle
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetModuleHandle(
+            string lpModuleName
+        );
+
+        // CreateRemoteThread signture https://www.pinvoke.net/default.aspx/kernel32.createremotethread
+        [DllImport("kernel32.dll")]
+        public static extern IntPtr CreateRemoteThread(
+            IntPtr hProcess,
+            IntPtr lpThreadAttributes,
+            uint dwStackSize,
+            IntPtr lpStartAddress,
+            IntPtr lpParameter,
+            uint dwCreationFlags,
+            IntPtr lpThreadId
+        );
+
+        // CloseHandle signture https://www.pinvoke.net/default.aspx/kernel32.closehandle
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
+        [SuppressUnmanagedCodeSecurity]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool CloseHandle(
+            IntPtr hObject
+        );
+    }
+}
--- DllInjection/DllInjection/Program.cs
+++ DllInjection/DllInjection/Program.cs
@@ -10,114 +10,6 @@ namespace DllInjection
 {
     class Program
     {
-        // OpenProcess signture https://www.pinvoke.net/default.aspx/kernel32.openprocess
-        [Flags]
-        public enum ProcessAccessFlags : uint
-        {
-            All = 0x001F0FFF,
-            Terminate = 0x00000001,
-            CreateThread = 0x00000002,
-            VirtualMemoryOperation = 0x00000008,
-            VirtualMemoryRead = 0x00000010,
-            VirtualMemoryWrite = 0x00000020,
-            DuplicateHandle = 0x00000040,
-            CreateProcess = 0x000000080,
-            SetQuota = 0x00000100,
-            SetInformation = 0x00000200,
-            QueryInformation = 0x00000400,
-            QueryLimitedInformation = 0x00001000,
-            Synchronize = 0x00100000
-        }
-
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern IntPtr OpenProcess(
-        ProcessAccessFlags processAccess,
-        bool bInheritHandle,
-        int processId);
-        public static IntPtr OpenProcess(Process proc, ProcessAccessFlags flags)
-        {
-            return OpenProcess(flags, false, proc.Id);
-        }
-
-        // VirtualAllocEx signture https://www.pinvoke.net/default.aspx/kernel32.virtualallocex
-        [Flags]
-        public enum AllocationType
-        {
-            Commit = 0x1000,
-            Reserve = 0x2000,
-            Decommit = 0x4000,
-            Release = 0x8000,
-            Reset = 0x80000,
-            Physical = 0x400000,
-            TopDown = 0x100000,
-            WriteWatch = 0x200000,
-            LargePages = 0x20000000
-        }
-
-        // VirtualFreeEx signture  https://www.pinvoke.net/default.aspx/kernel32.virtualfreeex
-        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-        static extern bool VirtualFreeEx(IntPtr hProcess, IntPtr lpAddress,
-        int dwSize, AllocationType dwFreeType);
-
-        [Flags]
-        public enum MemoryProtection
-        {
-            Execute = 0x10,
-            ExecuteRead = 0x20,
-            ExecuteReadWrite = 0x40,
-            ExecuteWriteCopy = 0x80,
-            NoAccess = 0x01,
-            ReadOnly = 0x02,
-            ReadWrite = 0x04,
-            WriteCopy = 0x08,
-            GuardModifierflag = 0x100,
-            NoCacheModifierflag = 0x200,
-            WriteCombineModifierflag = 0x400
-        }
-
-        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
-        static extern IntPtr VirtualAllocEx(
-            IntPtr hProcess,
-            IntPtr lpAddress,
-            IntPtr dwSize,
-            AllocationType flAllocationType,
-            MemoryProtection flProtect);
-
-        // WriteProcessMemory signture https://www.pinvoke.net/default.aspx/kernel32/WriteProcessMemory.html
-        [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool WriteProcessMemory(
-        IntPtr hProcess,
-        IntPtr lpBaseAddress,
-        [MarshalAs(UnmanagedType.AsAny)] object lpBuffer,
-        int dwSize,
-        out IntPtr lpNumberOfBytesWritten);
-
-        // GetProcAddress signture https://www.pinvoke.net/default.aspx/kernel32.getprocaddress
-        [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
-        static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
-
-        // GetModuleHandle signture http://pinvoke.net/default.aspx/kernel32.GetModuleHandle
-        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
-        public static extern IntPtr GetModuleHandle(string lpModuleName);
-
-        // CreateRemoteThread signture https://www.pinvoke.net/default.aspx/kernel32.createremotethread
-        [DllImport("kernel32.dll")]
-        static extern IntPtr CreateRemoteThread(
-        IntPtr hProcess,
-        IntPtr lpThreadAttributes,
-        uint dwStackSize,
-        IntPtr lpStartAddress,
-        IntPtr lpParameter,
-        uint dwCreationFlags,
-        IntPtr lpThreadId);
-
-        // CloseHandle signture https://www.pinvoke.net/default.aspx/kernel32.closehandle
-        [DllImport("kernel32.dll", SetLastError = true)]
-        [ReliabilityContract(Consistency.WillNotCorruptState, Cer.Success)]
-        [SuppressUnmanagedCodeSecurity]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool CloseHandle(IntPtr hObject);
-
         static void Main(string[] args)
         {
             // Check arguments
@@ -134,10 +26,7 @@ namespace DllInjection
             IntPtr Size = (IntPtr)DllPath.Length;
 
             // Make sure file exist
-            if (File.Exists(DllPath))
-            {
-            }
-            else
+            if (File.Exists(DllPath) == false)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("[!] File {0} does not exist. Please check file path.", DllPath);
@@ -174,10 +63,12 @@ namespace DllInjection
             }
 
             // Open handle to the target process
-            IntPtr ProcHandle = OpenProcess(
+            IntPtr ProcHandle = Native.OpenProcess(
                 ProcessAccessFlags.All,
                 false,
-                ProcId);
+                ProcId
+            );
+
             if (ProcHandle == null)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -193,12 +84,13 @@ namespace DllInjection
             }
 
             // Allocate DLL space
-            IntPtr DllSpace = VirtualAllocEx(
+            IntPtr DllSpace = Native.VirtualAllocEx(
                 ProcHandle,
                 IntPtr.Zero,
                 Size,
                 AllocationType.Reserve | AllocationType.Commit, 
-                MemoryProtection.ExecuteReadWrite);
+                MemoryProtection.ExecuteReadWrite
+            );
 
             if (DllSpace == null)
             {
@@ -216,13 +108,13 @@ namespace DllInjection
 
             // Write DLL content to VAS of target process
             byte[] bytes = Encoding.ASCII.GetBytes(DllPath);
-            bool DllWrite = WriteProcessMemory(
+            bool DllWrite = Native.WriteProcessMemory(
                 ProcHandle,
                 DllSpace,
                 bytes,
                 (int)bytes.Length,
                 out var bytesread
-                );
+            );
 
             if (DllWrite == false)
             {
@@ -239,8 +131,8 @@ namespace DllInjection
             }
 
             // Get handle to Kernel32.dll and get address for LoadLibraryA
-            IntPtr Kernel32Handle = GetModuleHandle("Kernel32.dll");
-            IntPtr LoadLibraryAAddress = GetProcAddress(Kernel32Handle, "LoadLibraryA");
+            IntPtr Kernel32Handle = Native.GetModuleHandle("Kernel32.dll");
+            IntPtr LoadLibraryAAddress = Native.GetProcAddress(Kernel32Handle, "LoadLibraryA");
 
             if (LoadLibraryAAddress == null)
             {
@@ -257,7 +149,7 @@ namespace DllInjection
             }
 
             // Create remote thread in the target process
-            IntPtr RemoteThreadHandle = CreateRemoteThread(
+            IntPtr RemoteThreadHandle = Native.CreateRemoteThread(
                 ProcHandle,
                 IntPtr.Zero,
                 0,
@@ -265,7 +157,7 @@ namespace DllInjection
                 DllSpace,
                 0,
                 IntPtr.Zero
-                );
+            );
 
             if (RemoteThreadHandle == null)
             {
@@ -282,11 +174,13 @@ namespace DllInjection
             }
 
             // Deallocate memory assigned to DLL
-            bool FreeDllSpace = VirtualFreeEx(
+            bool FreeDllSpace = Native.VirtualFreeEx(
                 ProcHandle,
                 DllSpace,
                 0,
-                AllocationType.Release);
+                AllocationType.Release
+            );
+
             if (FreeDllSpace == false)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -302,10 +196,10 @@ namespace DllInjection
             }
 
             // Close remote thread handle
-            CloseHandle(RemoteThreadHandle);
+            Native.CloseHandle(RemoteThreadHandle);
 
             // Close target process handle
-            CloseHandle(ProcHandle);
+            Native.CloseHandle(ProcHandle);
         }
     }
 }
