fe4aac294ac7cbc4582cadde23f3443b6c5708a4
Thomas Leiber <tomthebig1337@gmail.com>
2021-03-01 19:05:49 +0100

    * minor refactoring

--- DllInjection/ManagedMessageBox/EntryPoint.cs
+++ DllInjection/ManagedMessageBox/EntryPoint.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace ManagedMessageBox
 {
